371c35d7c29f05c0ad363343c6f1d4065043cfa5
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-25 10:17:09 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = ""
+# -------------------------------------------------------------
+API_HASH = ""
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("7653249811:AAHykuzw1rYRNqPpDD20bynfIf9jgyuahTo", None)
+MONGO_URL = getenv("mongodb+srv://tcronebtech2015: tcroneb net@cluster0.1nanqam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+OWNER_ID = int(getenv("7080079152", "7080079152"))
+SUPPORT_GRP = "LoveisgoneForme_2"
+UPDATE_CHNL = "paidtechzone"
+OWNER_USERNAME = "tcronebnet"
+    
